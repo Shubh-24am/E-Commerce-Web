b3035f6125b6cd0cc10193e9dfcd04329e378798
Shubh-24am <ishubhamgupta99@gmail.com>
2026-02-18 14:30:49 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
- origin:["https://e-commerce-web-frontendtwo.onrender.com" , "http://localhost:5175", "http://localhost:5174"],
+ origin:["https://e-commerce-web-frontendtwo.onrender.com" , "https://e-commerce-web-admin-wwae.onrender.com"],
  credentials:true
 }))
 
